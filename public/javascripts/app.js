cc593eb2c178065aad0d48c7a694cca1c02f09ff
Alex Perkowski <perkowskialex@gmail.com>
2020-01-07 09:47:05 -0600

    chat is functioning

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('dom loaded')
+
+let username = document.getElementById('username')
+let message = document.getElementById('message')
+let send = document.getElementById('send')
+
 var socket = io();
-console.log(socket);
-
-let username = document.querySelector('username')
-let message = document.querySelector('message')
-let send = document.querySelector('send')
-
 socket.on('send-message',function(data){
-    addMesasge(data)
+    addMessage(data)
 })
 
-send.addEventListener('click', function(evt){
+send.addEventListener('click', function(){
     socket.emit('send-message', {
         name:username.value,
         msg:message.value
@@ -18,5 +19,6 @@ send.addEventListener('click', function(evt){
 })
 
 function addMessage(data) {
-    messages.innerHTML += ['<li>', data.name, ':', data.msg + '</li>'].join('')
+    messages.innerHTML += ['<li>', data.name, ': ', data.msg + '</li>'].join('')
 }
+})
